e0b091a0c48690e7d351b6d8af29fe27906598e9
Jair Hernandez <jairhernandez_r@icloud.com>
2026-05-09 11:41:39 -0500

    Primera version inicial
    probando musica

---
+++ app.js
@@ -0,0 +1,65 @@
+// FECHA DEL EVENTO
+const weddingDate = new Date("Jul 20, 2026 17:00:00").getTime();
+const music = document.getElementById("music");
+const btn = document.getElementById("musicBtn");
+
+let playing = false;
+
+// activar con botón
+btn.addEventListener("click", () => {
+  if(!playing){
+    music.play();
+    btn.innerHTML = "🔊";
+    playing = true;
+  }else{
+    music.pause();
+    btn.innerHTML = "🔇";
+    playing = false;
+  }
+});
+
+// activar automáticamente al primer clic en pantalla
+document.body.addEventListener("click", () => {
+  if(!playing){
+    music.play();
+    playing = true;
+  }
+}, { once: true });
+
+document.addEventListener("mousemove", startMusic);
+document.addEventListener("scroll", startMusic);
+document.addEventListener("touchstart", startMusic);
+
+function startMusic() {
+  const music = document.getElementById("music");
+  music.play();
+
+  document.removeEventListener("mousemove", startMusic);
+  document.removeEventListener("scroll", startMusic);
+  document.removeEventListener("touchstart", startMusic);
+}
+
+const interval = setInterval(() => {
+
+  const now = new Date().getTime();
+  const distance = weddingDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+}, 1000);
+
+
+// RSVP
+document.getElementById("rsvpForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  alert("Gracias por confirmar tu asistencia ❤️");
+});
