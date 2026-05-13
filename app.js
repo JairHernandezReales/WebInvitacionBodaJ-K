7ed4b7796d631f25294327ae821b4d092e340aec
Jair Hernandez <jairhernandez_r@icloud.com>
2026-05-13 17:24:56 -0500

    Actualizacion Final
    Se mejoro los modales, se conecto a google form directo a la web, se agrego el inicio por codigo

--- app.js
+++ app.js
@@ -69,3 +69,82 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 reveals.forEach(el => observer.observe(el));
+
+
+//  Conexion a google form desde js
+
+document.getElementById("formAsistencia").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  let form = this;
+
+  let datos = new FormData();
+
+  datos.append("entry.2092238618", form.nombre.value);
+  datos.append("entry.2000174007", form.acompanantes.value);
+  datos.append("entry.1495685309", form.nombresAcompanantes.value);
+  datos.append("entry.1653027947", form.asistencia.value);
+  datos.append("entry.1557746772", form.regalo.value);
+  datos.append("entry.1321647054", form.mensaje.value);
+  datos.append("entry.913320467", form.cancion.value);
+
+  fetch("https://docs.google.com/forms/d/e/1FAIpQLSf9q_Q3aaAsLtTI6okYOaFOTrupnUzv48NzxCAqzbBCZIctTg/formResponse", {
+    method: "POST",
+    mode: "no-cors",
+    body: datos
+  });
+
+  form.reset();
+  cerrarFormulario();
+});
+
+function abrirFormulario(){
+  document.getElementById("modalFormulario").style.display = "block";
+
+  if(window.nombreInvitado){
+    document.getElementById("nombreInput").value = window.nombreInvitado;
+  }
+
+  // 🔥 generar opciones según el invitado
+  generarOpcionesAcompanantes();
+}
+
+function cerrarFormulario() {
+  document.getElementById("modalFormulario").style.display = "none";
+}
+
+// Cerrar al hacer clic fuera
+window.onclick = function(e) {
+  let modal = document.getElementById("modalFormulario");
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+};
+
+function abrirGracias(){
+  document.getElementById("modalGracias").style.display = "block";
+}
+
+function cerrarGracias(){
+  document.getElementById("modalGracias").style.display = "none";
+}
+
+// Capturar envío
+document.getElementById("formAsistencia").addEventListener("submit", function(e) {
+  e.preventDefault();
+  cerrarFormulario();
+  abrirGracias();
+});
+
+// ABRIR / CERRAR MODAL
+function abrirPin() {
+  document.getElementById("modalPin").style.display = "block";
+}
+
+function cerrarPin() {
+  document.getElementById("modalPin").style.display = "none";
+}
+
+function mostrarBienvenida(nombre){
+  document.getElementById("bienvenido").innerText = "Bienvenido/a " + nombre + " 🌻";
+}
